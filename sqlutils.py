98ffae4c7bac602460b352ba28b6008adc55eed1
Jeremy A <jeremy@obsoleter.com>
2012-08-29 09:27:11 -0400

    simplify run method.  broken

--- sqlutils.py
+++ sqlutils.py
@@ -6,17 +6,51 @@ from functools import wraps
 
 logger = logging.getLogger('sqlutils')
 
+def caller_info():
+    """Return module and function name of function two levels
+    down the stack, so- the caller of whichever function called
+    this one"""
+    # inspect stack for caller module and function
+    func, mod, mod_name = None, None
+    caller = inspect.stack()[2]
+    try:
+        func = caller[3]
+        mod = inspect.getmodule(caller[0])
+        mod_name = getattr(mod, '__name__', '__main__')
+    finally:
+        del caller
+        del mod
+    return mod_name, func
+
 def flatten(l):
+    """Take a heterogeneous list which may contain sublists
+    and flatten them into a single stream of values"""
     for i in l:
-        if is_seq(i) and not isinstance(i, bytearray):
+        if is_seq(i):
             for j in flatten(i):
                 yield j
         else:
             yield i
 
+def tuplify(l, modifier=None):
+    """Convert lists and sublists to tuples
+    with optional modifier of each element"""
+    new_l = []
+    for i in new_l:
+        if is_seq(i):
+            new_l.append(tuplify(i))
+        elif modifier:
+            new_l.append(modifier(i))
+        else:
+            new_l.append(i)
+    return tuple(new_l)
+
 def qmarks(l):
     """Create a string of question marks separated by commas"""
-    return '(%s)' % ','.join(len(l) * '?')
+    if is_seq(l):
+        return '(%s)' % ','.join(len(l) * '?')
+    else:
+        return '(?)'
 
 def query_in_location(query):
     """Get the parameter locations of IN components of an sql query"""
@@ -31,7 +65,10 @@ def query_in_location(query):
 
 def is_seq(item):
     """Check for sequences"""
-    return True if getattr(item, '__iter__', None) else False
+    if getattr(item, '__iter__', None) and not isinstance(item, bytearray):
+        return True
+    else:
+        return False
 
 def remove_ws(query):
     """Remove excessive whitespace from string"""
@@ -86,40 +123,34 @@ class DbConnections(object):
         for k, conn_string in kwargs.iteritems():
             self.conns[k] = pyodbc.connect(conn_string, autocommit=True)
             self.conns[k].add_output_converter(pyodbc.SQL_BINARY, h)
-            
-    def _process_params(self, params):
-        new_params = []
-        if is_seq(params):
-            for p in params:
-                new_params.append(p if not is_seq(p) else tuple(p))
-        else:
-            new_params.append(params)
-        return tuple(new_params)
 
-    def run(self, query, params=()):
+    def _run(self, conn, query, params):
+        """Perform the actual query execution with the given
+        pyodbc connection, query string, and query parameters"""
+        cursor = conn.execute(query, params)
+        results = []
+        results.append([column[0] for column in cursor.description])
+        for row in cursor.fetchall():
+            results.append(row)
+        cursor.close()
+        return results
+
+    def run(self, query, *params):
         """Execute an SQL query
 
         Given a select query, query params and a connection
         string, execute the query and return the results as
         a list of dict, where the dict keys are the column names
         """
-        # inspect stack for caller module and function
-        func, mod = None, None
-        caller = inspect.stack()[1]
-        try:
-            func = caller[3]
-            mod = inspect.getmodule(caller[0])
-            mod_name = getattr(mod, '__name__', '__main__')
-        finally:
-            del caller
-            del mod
+        mod_name, func = caller_info()
 
-        # get connection ifor for caller
+        # get connection for for caller
         conn_name = self.funcs.get('%s.%s' % (mod_name, func))
         conn = self.conns.get(conn_name)
 
-        new_params = self._process_params(params)
-        cache_key = (conn_name, query, new_params)
+        # make params hashable, if possible
+        tupled_params = tuplify(params)
+        cache_key = (conn_name, query, tupled_params)
 
         # return cached results if present
         cached_results = self.cache.get(cache_key)
@@ -129,26 +160,17 @@ class DbConnections(object):
             return cached_results
 
         # translate hex params to bytearrays
-        hexed_params = []
-        for p in new_params:
-            if is_seq(p):
-                sub_params = []
-                for i in p:
-                    sub_params.append(b(i) if is_hex_string(i) else i)
-                hexed_params.append(tuple(sub_params))
-            elif is_hex_string(p):
-                hexed_params.append(b(p))
-            else:
-                hexed_params.append(p)
-        new_params = tuple(hexed_params)
+        hexed_params = tuplify(tupled_params,
+                               lambda i: b(i) if is_hex_string(i) else i)
 
         # get tokenized query and location of INs in query
         tokens, ins = query_in_location(query)
         for i, v in enumerate(ins):
             if v[1]:
-                tokens[v[0]] = qmarks(new_params[i])
+                tokens[v[0]] = qmarks(hexed_params[i])
 
         new_query = ' '.join(tokens)
+        flattened_params = list(flatten(hexed_params))
             
         if not conn:
             logger.debug('No connection chosen.  '
@@ -158,12 +180,7 @@ class DbConnections(object):
         else:
             logger.debug('Running sql: %s, %s' % (new_query,
                                                 repr(params)))
-            cursor = conn.execute(new_query, list(flatten(new_params)))
-            results = []
-            results.append([column[0] for column in cursor.description])
-            for row in cursor.fetchall():
-                results.append(row)
-            cursor.close()
+            results = self._run(conn, new_query, flattened_params)
             self.cache[cache_key] = results
             return results
 
