b497bfb4e301ed73fd4c6b9ddf04103ddbe08f45
Jeremy A <jeremy@obsoleter.com>
2012-09-10 11:57:47 -0400

    bump setup.py version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='sqlutils',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple pyodbc helper methods',
     long_description=readme,
     author='Jeremy Axmacher',
