4060dd061517e4d2bd635d6e8a7ec6716dd944d0
Jeremy A <jeremy@obsoleter.com>
2012-09-10 10:57:30 -0400

    add license. add setuptools setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('readme.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sqlutils',
+    version='0.0.1',
+    description='Simple pyodbc helper methods',
+    long_description=readme,
+    author='Jeremy Axmacher',
+    author_email='jeremy@obsoleter.com',
+    url='https://github.com/obsoleter/sqlutils',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
