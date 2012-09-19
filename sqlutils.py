dce44617b150aa3d6fe10b93176f0d061d187784
Jeremy A <jeremy@obsoleter.com>
2012-09-19 17:43:14 -0400

    pep8/pylint refactor.

--- sqlutils.py
+++ sqlutils.py
@@ -1,16 +1,20 @@
-import re
+"""
+Basic pyodbc helper methods for simplifying sql statements
+and caching query results.
+"""
 import logging
 import pyodbc
-import inspect
-from exceptions import Exception
-from funcutils import *
+from funcutils import memoize, flatten, tuplify, is_seq, remove_ws, \
+    chunks, pipe
 
-logger = logging.getLogger('sqlutils')
-    
+LOGGER = logging.getLogger('sqlutils')
+
+
 def is_hex_string(s):
     """Test for "hex"-ness of a string"""
     return (isinstance(s, str) or isinstance(s, unicode)) \
-           and len(s) > 2 and s[:2] == '0x' and len(s) % 2 == 0
+        and len(s) > 2 and s[:2] == '0x' and len(s) % 2 == 0
+
 
 def hextobytes(hs):
     """Convert a hex string to bytearray
@@ -21,11 +25,12 @@ def hextobytes(hs):
     """
     try:
         return bytearray([int(c, 16) for c in chunks(hs[2:], 2)])
-    except:
+    except ValueError:
         return hs
 
+
 def bytestohex(bs):
-    """Convert bytearray to hex string  
+    """Convert bytearray to hex string
 
     Given a bytearray, convert it to a string of
     an even length, containing the associated
@@ -34,55 +39,65 @@ def bytestohex(bs):
     try:
         hs = ["{0:0>2}".format(hex(b)[2:].upper()) for b in bs]
         return '0x' + ''.join(hs)
-    except:
+    except TypeError:
         return bs
+
 
 def hex_tupler(l):
     """Tuplify a list, converting hex strings into bytes"""
-    return tuplify(l, lambda i: hextobytes(i) \
-                      if is_hex_string(i) \
-                      else i)
-    
+    return tuplify(l,
+                   lambda i: hextobytes(i) if is_hex_string(i) else i)
+
+
+def questionmarks(l):
+    """Create a string of question marks separated by commas"""
+    if is_seq(l):
+        length = len(l) if len(l) > 0 else 1
+        return '(%s)' % ','.join(length * '?')
+    else:
+        return '(?)'
+
+
+def query_in_location(query):
+    """Get the parameter locations of IN components of an sql query"""
+    tokens = query.split()
+    in_params = []
+    for i in xrange(len(tokens)):
+        if tokens[i] == '(?)':
+            in_params.append((i, True))
+        elif tokens[i] == '?':
+            in_params.append((i, False))
+    return (tokens, in_params)
+
+
+def reparamaterize_query(query, params):
+    """Add question marks as needed to support the use
+    of the IN clause in the query"""
+    tokens, ins = query_in_location(query)
+    for i, v in enumerate(ins):
+        if v[1]:
+            tokens[v[0]] = questionmarks(params[i])
+    return ' '.join(tokens)
+
+
 class SqlQueryParamsError(Exception):
+    """The parameters indicated in the query do not match up
+    with the supplied arguments."""
     pass
+
 
 class NoResultSetError(Exception):
+    """No result sets were returned for the query."""
     pass
 
+
 class DbConnection(object):
+    """Class used to wrap pyodbc connections."""
     def __init__(self, connection_string):
         """Initialize the connection string and connection"""
         self.connection_string = connection_string
         self.conn = None
 
-    def _questionmarks(self, l):
-        """Create a string of question marks separated by commas"""
-        if is_seq(l):
-            length = len(l) if len(l) > 0 else 1
-            return '(%s)' % ','.join(length * '?')
-        else:
-            return '(?)'
-
-    def _query_in_location(self, query):
-        """Get the parameter locations of IN components of an sql query"""
-        tokens = query.split()
-        in_params = []
-        for i in xrange(len(tokens)):
-            if tokens[i] == '(?)':
-                in_params.append((i, True))
-            elif tokens[i] == '?':
-                in_params.append((i, False))
-        return (tokens, in_params)
-
-    def _reparamaterize_query(self, query, params):
-        """Add question marks as needed to support the use
-        of the IN clause in the query"""
-        tokens, ins = self._query_in_location(query)
-        for i, v in enumerate(ins):
-            if v[1]:
-                tokens[v[0]] = self._questionmarks(params[i])
-        return ' '.join(tokens)
-        
     def _query_key_maker(self, *args, **kwargs):
         """Generate hashable keys for the query method
 
@@ -92,14 +107,16 @@ class DbConnection(object):
         arg_copy = list(args)
         query = remove_ws(arg_copy.pop(0)).lower()
         tupled_params = tuplify(arg_copy)
-        kwarg_copy = tuple([(k,v) for k,v in kwargs.iteritems()])
+        kwarg_copy = tuple([(k, v) for k, v in kwargs.iteritems()])
         return (self.connection_string.lower(), query, tupled_params,
                 kwarg_copy)
-        
-    def _submit(self, query, params, headers=False, results=True,
-                dictify=False):
+
+    def direct(self, query, params, **kwargs):
         """Perform the actual query execution with the given
         pyodbc connection, query string, and query parameters"""
+        headers = kwargs.get('headers', False)
+        results = kwargs.get('results', True)
+        dictify = kwargs.get('dictify', False)
         if not self.conn:
             self.conn = pyodbc.connect(self.connection_string,
                                        autocommit=True)
@@ -111,7 +128,7 @@ class DbConnection(object):
             while not cursor.description and cursor.nextset():
                 pass
             if not cursor.description:
-                logger.debug('No resultset for query %s' % query)
+                LOGGER.debug('No resultset for query %s' % query)
                 raise NoResultSetError()
 
             results = []
@@ -141,8 +158,8 @@ class DbConnection(object):
         Given a select query, query params, a choice of getting
         column headers, and of getting results at all,
         translate the params to bytearrays as necessary and tuplify
-        them.  
-        
+        them.
+
         Also, modify the query string to account for uses of
         the IN clause in the query, matching the number ofparameters
         (ie, question marks) in the query string to the number of
@@ -153,18 +170,18 @@ class DbConnection(object):
         headers = kwargs.get('headers', False)
         results = kwargs.get('results', True)
         dictify = kwargs.get('dictify', False)
-        new_query = self._reparamaterize_query(query, params)
+        new_query = reparamaterize_query(query, params)
         new_params = pipe(params, [
             hex_tupler,
             flatten,
             list
         ])
         if not new_query.count('?') == len(new_params):
-            logger.debug('Parameters supplied do not match query.'
+            LOGGER.debug('Parameters supplied do not match query.'
                          ' Raising exception.')
             raise SqlQueryParamsError()
 
-        logger.debug('Running sql: %s, %s' % (new_query, repr(params)))
-        results = self._submit(new_query, new_params, headers=headers,
-                               results=results, dictify=dictify)
+        LOGGER.debug('Running sql: %s, %s' % (new_query, repr(params)))
+        results = self.direct(new_query, new_params, headers=headers,
+                              results=results, dictify=dictify)
         return results
